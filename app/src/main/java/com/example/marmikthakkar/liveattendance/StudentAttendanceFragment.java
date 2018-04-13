0a836877a5c80fd91d6ba8d3b6216faedef2013a
dakshparekh <dakshparekh@rediffmail.com>
2018-04-13 21:51:43 +0530

    passed object to student attendance fragment

--- app/src/main/java/com/example/marmikthakkar/liveattendance/StudentAttendanceFragment.java
+++ app/src/main/java/com/example/marmikthakkar/liveattendance/StudentAttendanceFragment.java
@@ -4,6 +4,7 @@ package com.example.marmikthakkar.liveattendance;
         import android.app.Fragment;
         import android.provider.ContactsContract;
         import android.util.Log;
+        import android.util.TimingLogger;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
@@ -18,17 +19,18 @@ package com.example.marmikthakkar.liveattendance;
         import com.google.firebase.database.ValueEventListener;
 
 
-public class StudentAttendanceFragment extends Fragment implements ValueEventListener{
+public class StudentAttendanceFragment extends Fragment{
 
     View view;
     User user;
     ListView listView;
     DatabaseReference mDatabaseRef;
+    TimingLogger timings;
     int maxhours;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         view = inflater.inflate(R.layout.fragment_student_attendance, container, false);
-
+        timings = new TimingLogger("exec", "methodA");
         if ( getArguments().getParcelable("User") != null ){
             user = getArguments().getParcelable("User");
         }else {
@@ -37,35 +39,38 @@ public class StudentAttendanceFragment extends Fragment implements ValueEventLis
         }
         listView = view.findViewById(R.id.listView);
         mDatabaseRef = FirebaseDatabase.getInstance().getReference();
-        Query query = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()).orderByValue();
-        query.addListenerForSingleValueEvent(this);
+        timings.addSplit("Getting references");
+        mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/"+"subjects");
 
         return view;
     }
 
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot) {
-        if(dataSnapshot.exists()){
-            for (DataSnapshot subject: dataSnapshot.getChildren()){
-                Log.d("subject", subject.child("faculty").getValue().toString());
-                if (subject.child("total").exists()){
-                    maxhours = Integer.parseInt(subject.child("total").child("maxhours").getValue().toString());
-                    Log.d("max_hours", subject.getKey().toString());
-//                    Query query = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/"+subject.getKey()+"/attendance").orderByChild(user.getUid()).equalTo(user.getUid());
-                    for (DataSnapshot uid: subject.child("total/attendance").getChildren()){
-                        if (uid.getKey().toString().equals(user.getUid())){
-                            Toast.makeText(getActivity(), uid.getValue().toString(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-            }
-        }else{
-            Toast.makeText(getActivity(), "Data Not Found", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-
-    }
+//    @Override
+//    public void onDataChange(DataSnapshot dataSnapshot) {
+//        timings.addSplit("Getting snapshot");
+//        if(dataSnapshot.exists()){
+//            for (DataSnapshot subject: dataSnapshot.getChildren()){
+//                Log.d("faculty", subject.child("faculty").getValue().toString());
+//                if (subject.child("total").exists()){
+//                    maxhours = Integer.parseInt(subject.child("total").child("maxhours").getValue().toString());
+//                    Log.d("max_hours", subject.getKey().toString());
+////                    Query query = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/"+subject.getKey()+"/attendance").orderByChild(user.getUid()).equalTo(user.getUid());
+//                    for (DataSnapshot uid: subject.child("total/attendance").getChildren()){
+//                        if (uid.getKey().toString().equals(user.getUid())){
+//                            Toast.makeText(getActivity(), uid.getValue().toString(), Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                }
+//            }
+//            timings.addSplit("display");
+//            timings.dumpToLog();
+//        }else{
+//            Toast.makeText(getActivity(), "Data Not Found", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    @Override
+//    public void onCancelled(DatabaseError databaseError) {
+//
+//    }
 }
