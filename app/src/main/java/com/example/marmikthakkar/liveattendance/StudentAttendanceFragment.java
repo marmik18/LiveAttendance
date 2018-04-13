2e9cf65cdb104fe485be4f2cf3fea04af480bfba
marmik18 <marmik18@gmail.com>
2018-04-13 23:55:58 +0530

    getting total and attendance from every subject to calculate the percent.

--- app/src/main/java/com/example/marmikthakkar/liveattendance/StudentAttendanceFragment.java
+++ app/src/main/java/com/example/marmikthakkar/liveattendance/StudentAttendanceFragment.java
@@ -8,6 +8,7 @@ package com.example.marmikthakkar.liveattendance;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.ListView;
+        import android.widget.TextView;
         import android.widget.Toast;
 
         import com.google.firebase.database.DataSnapshot;
@@ -17,14 +18,19 @@ package com.example.marmikthakkar.liveattendance;
         import com.google.firebase.database.Query;
         import com.google.firebase.database.ValueEventListener;
 
+        import java.lang.reflect.Array;
+        import java.util.ArrayList;
 
-public class StudentAttendanceFragment extends Fragment implements ValueEventListener{
+
+public class StudentAttendanceFragment extends Fragment{
 
     View view;
     User user;
     ListView listView;
+    TextView textView;
     DatabaseReference mDatabaseRef;
-    int maxhours;
+    ArrayList<Integer> maxHoursPerSubject;
+    ArrayList<Integer> attended;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         view = inflater.inflate(R.layout.fragment_student_attendance, container, false);
@@ -35,37 +41,65 @@ public class StudentAttendanceFragment extends Fragment implements ValueEventLis
             Toast.makeText(getActivity(), "Invalid Login", Toast.LENGTH_SHORT).show();
             getActivity().finish();
         }
+        textView = view.findViewById(R.id.testTextView);
         listView = view.findViewById(R.id.listView);
         mDatabaseRef = FirebaseDatabase.getInstance().getReference();
-        Query query = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()).orderByValue();
-        query.addListenerForSingleValueEvent(this);
+        Query subjectQuery = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/subjects");
+        subjectQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    maxHoursPerSubject = new ArrayList<>();
+                    attended = new ArrayList<>();
 
-        return view;
-    }
+                    for (DataSnapshot subject: dataSnapshot.getChildren()){
+                        Log.d("subject", subject.getValue().toString());
+                        Query attendance = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/"+subject.getValue().toString()+"/total/attendance/").orderByKey().equalTo(user.getUid());
+                        attendance.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                attended.add(Integer.parseInt(dataSnapshot.child(user.getUid()).getValue().toString()));
+                                Log.d("attendance", dataSnapshot.child(user.getUid()).getValue().toString());
+                            }
 
-    @Override
-    public void onDataChange(DataSnapshot dataSnapshot) {
-        if(dataSnapshot.exists()){
-            for (DataSnapshot subject: dataSnapshot.getChildren()){
-                Log.d("subject", subject.child("faculty").getValue().toString());
-                if (subject.child("total").exists()){
-                    maxhours = Integer.parseInt(subject.child("total").child("maxhours").getValue().toString());
-                    Log.d("max_hours", subject.getKey().toString());
-//                    Query query = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/"+subject.getKey()+"/attendance").orderByChild(user.getUid()).equalTo(user.getUid());
-                    for (DataSnapshot uid: subject.child("total/attendance").getChildren()){
-                        if (uid.getKey().toString().equals(user.getUid())){
-                            Toast.makeText(getActivity(), uid.getValue().toString(), Toast.LENGTH_SHORT).show();
-                        }
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+                        Query maxhours = mDatabaseRef.child("courses/"+user.getCourse()+"/"+user.getProgramme()+"/"+user.getSem()+"/"+subject.getValue().toString()+"/total/maxhours");
+                        maxhours.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                maxHoursPerSubject.add(Integer.parseInt(dataSnapshot.getValue().toString()));
+                                textView.setText("");
+                                for (int i=0; i<attended.size();i++){
+                                    textView.append(attended.get(i).toString()+"\t"+maxHoursPerSubject.get(i).toString());
+                                    textView.append("\n");
+                                }
+                                Log.d("maxhours", dataSnapshot.getValue().toString());
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
                     }
+                }else{
+                    Toast.makeText(getActivity(), "Data Not Found", Toast.LENGTH_SHORT).show();
                 }
             }
-        }else{
-            Toast.makeText(getActivity(), "Data Not Found", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+//        Toast.makeText(getActivity(), maxHoursPerSubject.toArray().toString(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getActivity(), attended.toArray().toString(), Toast.LENGTH_SHORT).show();
+        return view;
     }
 }
